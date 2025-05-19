83864f4876d15fb087fffced3f74631229edaac0
Veronic26 <thaliacampoverde26@gmail.com>
2025-05-19 18:06:29 -0500

    Add files

---
+++ Ejercicio 3/tablasSeleccionadas.js
@@ -0,0 +1,8 @@
+// tablasSeleccionadas.js
+
+// Pedimos al usuario si quiere ver tablas de números "pares" o "impares"
+let tipo = prompt("¿Qué tablas quieres ver? Escribe 'pares' o 'impares':");
+
+// Convertimos a minúsculas para evitar errores de mayúsculas
+tipo = tipo.toLowerCase();
+
