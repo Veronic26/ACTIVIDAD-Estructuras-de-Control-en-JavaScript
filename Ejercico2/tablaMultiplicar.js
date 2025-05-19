3798bbc33a46ed52dfa31dfadac7e4c43ffc00c7
Veronic26 <thaliacampoverde26@gmail.com>
2025-05-19 18:05:50 -0500

    Add files

---
+++ Ejercico2/tablaMultiplicar.js
@@ -0,0 +1,7 @@
+// tablaMultiplicar.js
+
+// Pedimos al usuario un número para generar su tabla de multiplicar
+let numero = prompt("Ingrese un número para ver su tabla de multiplicar:");
+
+// Convertimos el valor a número
+numero = Number(numero);
