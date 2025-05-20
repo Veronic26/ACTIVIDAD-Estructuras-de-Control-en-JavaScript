625cdfe89915f8262fb79064efb44a19486aa2dd
Veronic26 <thaliacampoverde26@gmail.com>
2025-05-19 19:59:51 -0500

    Update edadCategoria.js

--- Ejercicio 1/edadCategoria.js
+++ Ejercicio 1/edadCategoria.js
@@ -6,3 +6,15 @@ let edad = prompt("Por favor, ingresa tu edad:");
 // Convertimos la entrada a número
 edad = Number(edad);
 
+// Verificamos la categoría según la edad
+if (edad < 0) {
+    console.log("Edad no válida");
+} else if (edad <= 12) {
+    console.log("Niño");
+} else if (edad <= 17) {
+    console.log("Adolescente");
+} else if (edad <= 59) {
+    console.log("Adulto");
+} else {
+    console.log("Adulto mayor");
+}
