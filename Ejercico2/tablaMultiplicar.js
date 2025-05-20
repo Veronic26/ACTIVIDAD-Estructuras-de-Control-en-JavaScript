d61120bff2ccc0cdef52336d056620a0453dec46
Veronic26 <thaliacampoverde26@gmail.com>
2025-05-19 20:00:54 -0500

    Update tablaMultiplicar.js

--- Ejercico2/tablaMultiplicar.js
+++ Ejercico2/tablaMultiplicar.js
@@ -5,3 +5,11 @@ let numero = prompt("Ingrese un número para ver su tabla de multiplicar:");
 
 // Convertimos el valor a número
 numero = Number(numero);
+if (isNaN(numero)) {
+    console.log("Por favor, ingresa un número válido.");
+} else {
+    console.log(`Tabla de multiplicar del ${numero}:`);
+    for (let i = 1; i <= 12; i++) {
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
